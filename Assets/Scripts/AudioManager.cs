dd54c56085956f2a556e8085a97a4e9f0b4aa5ae
Theodoro <lucastheo.g.a@gmail.com>
2021-07-10 19:56:23 -0300

    Add AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private static AudioManager instance;
+
+    // Start is called before the first frame update
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("GM").AddComponent<AudioManager>();
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            DestroyImmediate(this);
+            return;
+        }
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        foreach( Sound s in sounds ){
+            gameObject.AddComponent<AudioSource>();
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound sound = GetSound(name);
+        if (sound == null) { return; }
+        if (sound.EndCycle())
+        {
+            sound.Reset();
+            sound.source.Play();
+        }
+        else
+        {
+            sound.AddCycle();
+        }
+       
+    }
+
+    public void Stop(string name)
+    {
+        Sound sound = GetSound(name);
+        if (sound == null) { return; }
+        sound.source.Stop();
+        sound.Reset();
+    }
+
+    public Sound GetSound( string name)
+    {
+        foreach (Sound s in sounds)
+        {
+            if (s.name == name)
+            {
+                return s;
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume;
+    [HideInInspector]
+    public AudioSource source;
+    public int totalCycles;
+
+    private int howManyCycles;
+    
+    public void AddCycle()
+    {
+        howManyCycles++;
+    } 
+
+    public bool EndCycle()
+    {
+        return howManyCycles == totalCycles-1;
+    }
+
+    public void Reset()
+    {
+        howManyCycles = 0;
+    }
+
+    public float TimeCourse()
+    {
+        return source.clip.length / totalCycles;
+    }
+}
